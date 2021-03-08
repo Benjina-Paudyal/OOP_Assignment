db3c69dd3357c08fd7ce8ef2e4eba74fe5a80a8e
Benjina Paudyal <benjina.paudyal@gmail.com>
2021-03-08 12:48:09 +0100

    Add project files.

---
+++ OOP_Assignment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace OOP_Assignment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
